f562454b82983c332205334b272f34786575c10d
Irvin <irvincampos76@gmail.com>
2025-04-14 11:23:43 -0700

    OAuth token practice

---
+++ src/test/java/org/IrvinCampos/AUthTest.java
@@ -0,0 +1,28 @@
+package org.IrvinCampos;
+
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class AUthTest {
+    @Test public void AuthorizationTokenTest() {
+        String response = given()
+                .formParam("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .formParam("client_secret","erZOWM9g3UtwNRj340YYaK_W")
+                .formParam("grant_type","client_credentials")
+                .formParam("scope","trust")
+                .when().log().all()
+                .post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
+        System.out.println(response);
+        JsonPath jsonPath = new JsonPath(response);
+        String accessToken = jsonPath.getString("access_token");
+        System.out.println(accessToken);
+
+        String response2 = given()
+                .queryParam("access_token",accessToken)
+                .when().log().all()
+                .get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+        System.out.println(response2);
+    }
+}
