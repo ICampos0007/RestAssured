b93a855dd10ab26709283e49a0db5d1e6a2041ca
Irvin <irvincampos76@gmail.com>
2025-04-22 12:18:34 -0700

    parsing complex JSON

--- src/test/java/org/IrvinCampos/AUthTest.java
+++ src/test/java/org/IrvinCampos/AUthTest.java
@@ -1,8 +1,15 @@
 package org.IrvinCampos;
 
 import io.restassured.path.json.JsonPath;
+import org.IrvinCampos.pojo.Api;
+import org.IrvinCampos.pojo.Courses;
+import org.IrvinCampos.pojo.GetCourse;
+import org.IrvinCampos.pojo.WebAutomation;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
 public class AUthTest {
@@ -19,10 +26,25 @@ public class AUthTest {
         String accessToken = jsonPath.getString("access_token");
         System.out.println(accessToken);
 
-        String response2 = given()
+        GetCourse getCourse = given()
                 .queryParam("access_token",accessToken)
                 .when().log().all()
-                .get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
-        System.out.println(response2);
+                .get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourse.class);
+
+        System.out.println(getCourse.getLinkedIn());
+        System.out.println(getCourse.getInstructor());
+        System.out.println(getCourse.getCourses().getApi().get(1).getCourseTitle());
+        List<Api> apiCourses = getCourse.getCourses().getApi();
+        for (int i =0; i < apiCourses.size(); i++) {
+            if (apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")) {
+                System.out.println(apiCourses.get(i).getPrice());
+            }
+        }
+
+        List<WebAutomation> getAllCourses = getCourse.getCourses().getWebAutomation();
+        for (int i= 0; i < getAllCourses.size(); i++) {
+            System.out.println(getAllCourses.get(i).getCourseTitle());
+        }
+
     }
 }
